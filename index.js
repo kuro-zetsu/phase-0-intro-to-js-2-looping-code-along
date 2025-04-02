b6cc9382ae5cbb7a60341e9dc4450d77f3ddfd49
Reagan Nyauma <jreagan2525@gmail.com>
2025-04-02 15:57:21 +0300

    used while and for loops

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArray = [];
+    for ( let i = 0 ; i < names.length ; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+console.log(writeCards(['mike', 'joe', 'doreen'], 'graduation'));
+
+
+const countDown = num => {
+    let counter = num;
+    while(counter >= 0) {
+        console.log(counter);
+        counter -=1;
+    }
+}
+
+countDown(10);
